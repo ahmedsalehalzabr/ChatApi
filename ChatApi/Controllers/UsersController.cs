9bc62a07675d86368d0efa610e54292998adf8ad
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-02-16 04:57:44 -1200

    Add project files.

---
+++ ChatApi/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using ChatApi.Data;
+using ChatApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public UsersController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+
+        public async Task< ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _db.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task< ActionResult<AppUser>> GetUserById(int id)
+        {
+            return await _db.Users.FindAsync(id);
+        }
+    }
+}
---
+++ ChatApi/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ChatApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions option) : base(option)
+        {
+            
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
