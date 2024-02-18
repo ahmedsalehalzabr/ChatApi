6af9c97e5393345aa5c83873ea0ae0aba03ba5e0
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-02-18 11:01:55 -1200

    3

--- ChatApi/Controllers/AccountController.cs
+++ ChatApi/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 ﻿using ChatApi.Data;
+using ChatApi.DTOs;
 using ChatApi.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -19,19 +21,24 @@ namespace ChatApi.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Regsiter(string username, string password)
+        public async Task<ActionResult<AppUser>> Regsiter(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
             _db.Users.Add(user);
             await _db.SaveChangesAsync();
             return Ok(user);
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await _db.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
--- ChatApi/DTOs/RegisterDto.cs
+++ ChatApi/DTOs/RegisterDto.cs
@@ -1,9 +1,13 @@
-﻿namespace ChatApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApi.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }
